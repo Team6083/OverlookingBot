fa0a686bf17bbdabf26c1d93c1f80a0036bf87d1
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2018-11-04 09:40:42 +0800

    add express router

--- src/index.js
+++ src/index.js
@@ -2,13 +2,49 @@ import 'babel-polyfill';
 
 const {RTMClient} = require('@slack/client');
 
-// An access token (from your Slack app or custom integration - usually xoxb)
+import config from 'config';
+import express from 'express';
+import http from 'http';
+
+import bootstrap from './bootstrap';
+import { log, normalizePort } from './utils';
+
 const token = process.env.SLACK_TOKEN;
 
-// The client is initialized and then started to get an active connection to the platform
-const rtm = new RTMClient(token);
-rtm.start();
+const app = express();
+app.start = async () => {
+  log.info('Starting Server...');
+  const port = normalizePort(config.get('port'));
+  app.set('port', port);
+  bootstrap(app);
+  const server = http.createServer(app);
 
-rtm.on('message', (event) => {
-  console.log(event);
+  server.on('error', (error) => {
+    if (error.syscall !== 'listen') throw error;
+    log.error(`Failed to start server: ${error}`);
+    process.exit(1);
+  });
+
+  server.on('listening', () => {
+    const address = server.address();
+    log.info(`Server listening ${address.address}:${address.port}`);
+  });
+
+  server.listen(port);
+};
+
+app.start().catch((err) => {
+  log.error(err);
 });
+
+export default app;
+
+
+
+// // The client is initialized and then started to get an active connection to the platform
+// const rtm = new RTMClient(token);
+// rtm.start();
+//
+// rtm.on('message', (event) => {
+//   console.log(event);
+// });
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+import { log } from './utils';
+
+const router = new express.Router();
+
+
+export default router;
