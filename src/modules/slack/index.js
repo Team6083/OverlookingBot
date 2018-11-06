0ed828810dd4417cdd289f0100edd58ca5c8358d
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2018-11-07 00:47:20 +0800

    add post handler of refresh users and channels

--- src/modules/slack/index.js
+++ src/modules/slack/index.js
@@ -1,12 +1,12 @@
 const {RTMClient, WebClient} = require('@slack/client');
 import {log} from '../../utils';
+import firebase from '../firebase/index'
 
 const token = process.env.SLACK_TOKEN;
 const rtm = new RTMClient(token);
 const web = new WebClient(token);
 
 export default function(slackConfig, db) {
-  const msgLogRef = db.collection('messages');
 
   rtm.start().catch((err) => {
     log.error(err);
@@ -15,11 +15,11 @@ export default function(slackConfig, db) {
   rtm.on('error', (err) => log.error(err));
 
   rtm.on('ready', (event) => {
-    console.log('Bot is ready.');
+    log.info('Bot is ready.');
   });
 
   rtm.on('message', (event) => {
-
+    const msgLogRef = db.collection('messages');
     const message = event;
 
     // Skip messages that are from a bot or my own user ID
@@ -29,12 +29,16 @@ export default function(slackConfig, db) {
     }
 
     // Log the message
-    console.log(`(channel:${message.channel}) ${message.user} says: ${message.text}`);
+    log.info(`(channel:${message.channel}) ${message.user} says: ${message.text}`);
     msgLogRef.add(message);
   });
 }
 
-module.export.getRTM = () => {
+module.exports.getRTM = () => {
   if (rtm.connected) return rtm;
   else return undefined;
 };
+
+module.exports.getWeb = () => {
+  return web;
+}
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,45 @@ import express from 'express';
 
 import {log} from './utils';
 
-const router = new express.Router();
+import {db} from './modules/firebase/index';
+import {getWeb} from './modules/slack/index';
 
+const router = new express.Router();
+const firestore = db();
+const webAPI = getWeb();
+
+router.post('/slack/refreshChannels', (req,res) => {
+  const dbchannelsList = firestore.collection('slack').doc('channels').collection('list');
+  webAPI.channels.list().then((resGet) => {
+    if(resGet.ok){
+      resGet.channels.forEach((channel) => {
+        dbchannelsList.doc(channel.id).set(channel);
+      });
+      res.sendStatus(200);
+    }
+    else{
+      log.wanrning(`Error occurred: ${resGet.error}`);
+      res.status(500).send('Internal Server Error:'+resGet.error);
+    }
+  });
+});
+
+router.post('/slack/refrechUsers', (req,res) => {
+  const dbusersList = firestore.collection('slack').doc('users').collection('list');
+  webAPI.users.list().then((resGet) => {
+    if(resGet.ok){
+      resGet.members.forEach((member) => {
+        if(!member.deleted){
+          dbusersList.doc(member.id).set(member);
+        }
+      });
+      res.sendStatus(200);
+    }
+    else{
+      log.wanrning(`Error occurred: ${resGet.error}`);
+      res.status(500).send('Internal Server Error:'+resGet.error);
+    }
+  });
+});
 
 export default router;
